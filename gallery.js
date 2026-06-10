5e60104b65b2029e4597fa69da1266f814c29871
Michael A. Coughlin <abereanone@pm.me>
2026-06-10 16:33:04 -0400

    initial update

---
+++ gallery.js
@@ -0,0 +1,41 @@
+/**
+ * GALLERY PHOTOS — Edit this file to add or remove photos from the homepage gallery.
+ *
+ * HOW TO ADD A PHOTO:
+ *   1. Upload your photo file into the "images/gallery/" folder on your website.
+ *   2. Add a new line to the GALLERY list below, following the same pattern.
+ *   3. Save this file and upload it to your website alongside the photo.
+ *
+ * CAPTION is optional — leave it as "" if you don't want a caption to appear.
+ *
+ * Example:
+ *   { src: "images/gallery/my-new-photo.jpg", caption: "Back porch in fall" },
+ */
+
+const GALLERY = [
+  { src: "images/gallery/13644670_640x480_500.jpg",   caption: "Hospitality House" },
+  { src: "images/gallery/13971576_4032x3024_500.jpg", caption: "" },
+  { src: "images/gallery/13644645_1512x2016_500.jpg", caption: "" },
+  { src: "images/gallery/13644650_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13644660_480x640_500.jpg",   caption: "" },
+  { src: "images/gallery/13644665_1200x1600_500.jpg", caption: "" },
+  { src: "images/gallery/13644771_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13644788_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13644838_640x480_500.jpg",   caption: "Actual Cleft of the Rock" },
+  { src: "images/gallery/13644853_4032x3024_500.jpg", caption: "" },
+  { src: "images/gallery/13644746_1600x1171_500.jpg", caption: "" },
+  { src: "images/gallery/13644777_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13644803_768x1024_500.jpg",  caption: "" },
+  { src: "images/gallery/13644843_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13644893_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13644903_2016x1512_500.jpg", caption: "" },
+  { src: "images/gallery/13644928_3024x4032_500.jpg", caption: "" },
+  { src: "images/gallery/13644939_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13972629_1024x768_500.jpg",  caption: "" },
+  { src: "images/gallery/13972676_768x1024_500.jpg",  caption: "" },
+  { src: "images/gallery/13972688_763x1170_500.jpg",  caption: "" },
+  { src: "images/gallery/13972696_360x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13972701_640x480_500.jpg",   caption: "" },
+  { src: "images/gallery/13972713_1170x1370_500.jpg", caption: "" },
+  { src: "images/gallery/22796188_443x590_500.jpg",   caption: "" },
+];
